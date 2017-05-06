ac946aa0e5ed43fe07bb0996a531ad2de56375aa
vtrust <vtrust32@gmail.com>
2017-05-06 21:12:31 +0800

    v0.0.3

--- pages/bookDetail/bookDetail.js
+++ pages/bookDetail/bookDetail.js
@@ -1,47 +1,130 @@
 Page({
     data: {
         book: {},
-        relatedBooks: [
-            { id: 'book2', title: 'hello2', cover: './cover/book2.jpg' },
-            { id: 'book3', title: 'hello3', cover: './cover/book3.jpg' },
-            { id: 'book1', title: 'hello1', cover: './cover/book1.jpg' },
-            { id: 'book3', title: 'hello3', cover: './cover/book3.jpg' },
-            { id: 'book1', title: 'hello1', cover: './cover/book1.jpg' },
-            { id: 'book2', title: 'hello2', cover: './cover/book2.jpg' },
-        ]
+        relatedBooks: [],
+        ifrelated: 0,
     },
-    onLoad: function (option) {
-        var that=this;
+    // 开启页面前
+    onLoad: function(option) {
+        var that = this;
         console.log(option.bookId);
         wx.request({
             url: 'http://localhost:3000/library/bookDetail',
             data: {
-                bookId: '123456'
+                bookId: '123'
             },
             method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
             // header: {}, // 设置请求的 header
-            success: function (res) {
+            success: function(res) {
                 console.log(res.data);
                 that.setData({
-                    book:res.data
+                    book: res.data.book,
+                    relatedBooks: res.data.relatedBooks
                 })
                 // success
             },
-            fail: function (res) {
+            fail: function(res) {
                 console.log('fail');
                 // fail
             },
-            complete: function (res) {
+            complete: function(res) {
                 // complete
             }
-        })
+        });
+
     },
-    bookDetail: function (e) {
+    // 获得书籍详情并更新数据
+    bookDetail: function(e) {
         wx.redirectTo({
             url: '/pages/bookDetail/bookDetail'
         })
     },
-    returnPersonal: function (e) {
+    // 书籍预约
+    reserve: function(e) {
+        var that = this;
+        console.log(e);
+        var that = this
+        wx.showToast({
+            title: '预约中',
+            icon: 'loading',
+            duration: 20000
+        })
+        wx.request({
+            url: 'http://localhost:3000/library/bookReserve', //仅为示例，并非真实的接口地址
+            data: {
+                bookId: e.target.dataset.bookid, //设置发送到后台的bookid
+            },
+            method: 'post',
+            header: {
+                'content-type': 'application/json'
+            },
+
+            success: function(res) {
+                console.log(res.data)
+                if (res.data == 'success') {
+                    wx.hideLoading();
+                    wx.showToast({
+                        title: '预约成功',
+                        icon: 'success'
+                    })
+                    that.setData({
+                        ifrelated: 1,
+                    })
+
+                } else {
+                    wx.hideLoading();
+                    wx.showModal({
+                        title: '提示',
+                        content: res.data
+                    })
+                }
+            }
+        })
+    },
+    // 取消预约
+    cancelReserve: function(e) {
+        var that = this;
+        console.log(e.target.dataset.bookid);
+        var that = this
+        wx.showToast({
+            title: '预约中',
+            icon: 'loading',
+            duration: 20000
+        })
+        wx.request({
+            url: 'http://localhost:3000/library/cancelReserve', //仅为示例，并非真实的接口地址
+            data: {
+                bookId: e.target.dataset.bookid, //设置发送到后台的bookid
+            },
+            method: 'post',
+            header: {
+                'content-type': 'application/json'
+            },
+
+            success: function(res) {
+                console.log(res.data)
+                if (res.data == 'success') {
+                    wx.hideLoading();
+                    wx.showToast({
+                        title: '取消成功',
+                        icon: 'success'
+                    })
+                    that.setData({
+                        ifrelated: 0,
+                    })
+
+                } else {
+                    wx.hideLoading();
+                    wx.showModal({
+                        title: '提示',
+                        content: res.data
+                    })
+                }
+            }
+        })
+    },
+    // 反回个人中心
+    returnPersonal: function(e) {
         wx.switchTab({
             url: '/pages/personal/personal'
         })
