48e8f3edb897a9c273df8d56623ba43e8b6db87d
vtrust <vtrust32@gmail.com>
2017-05-31 23:31:07 +0800

    t0.1.0

--- pages/news/news.js
+++ pages/news/news.js
@@ -16,19 +16,17 @@ Page({
         userId: getApp().globalData.userId
       },
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
       success: function (res) {
+        // success
         console.log(res);
         that.setData({
           messagesNum: res.data.messagesNum,
           messages: res.data.messages
         })
-
-        // success
       },
       fail: function (res) {
+         // fail
         console.log('fail');
-        // fail
       },
       complete: function (res) {
         // complete
@@ -36,14 +34,47 @@ Page({
     })
 
   },
-  deleteNews: function (e) {
+  deleteAllNews: function (e) {
     console.log(e);
+    var that = this;
+
+    wx.request({
+      url: host + '/personal/delAllNews', // 删除对应消息
+      data: {
+        userId: getApp().globalData.userId, //设置发送到后台的数据
+      },
+      method: 'post',
+      header: {
+        'content-type': 'application/json'
+      },
+
+      success: function (res) {
+        console.log(res.data)
+        if (res.data == 'success') {
+          wx.hideLoading();
+          wx.showToast({
+            title: '删除所有成功',
+            icon: 'success'
+          })
+          that.setData({
+            messages: [],
+            messagesNum:0
+            //ifrelated: 1
+          })
+
+        } else {
+          wx.hideLoading();
+          wx.showModal({
+            title: '提示',
+            content: res.data
+          })
+        }
+      }
+    })
   },
   deleteNew: function (e) {
     var that = this;
-    console.log(e);
     var param = {};
-
     var num = e.currentTarget.dataset.num;
     var string = "messages[" + num + "].ifShow";
     param[string] = 'changed data';
@@ -71,9 +102,7 @@ Page({
             title: '删除成功',
             icon: 'success'
           })
-          that.setData({
-            ifrelated: 1,
-          })
+
 
         } else {
           wx.hideLoading();
